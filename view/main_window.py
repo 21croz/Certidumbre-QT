fe0b5200113765c38f03706b83c5d7022830b74d
Cristobal Quiroz <cristobal.quiroz17@gmail.com>
2025-01-22 18:02:46 -0300

    segundo commit

--- view/main_window.py
+++ view/main_window.py
@@ -16,7 +16,9 @@ class MainWindow(QtWidgets.QMainWindow):
     Clase de la ventana principal.
     """
     def __init__(self):
-        """Constructor de la ventana principal."""
+        """
+        Constructor de la ventana principal.
+        """
         super(MainWindow, self).__init__()
 
         uic.loadUi(r'ui\main_window.ui', self)      # Cargar elementos graficos de la ventana
@@ -57,24 +59,24 @@ class MainWindow(QtWidgets.QMainWindow):
         self.entry_price_max = self.findChild(QtWidgets.QLineEdit, "entry_price_max")
         self.entry_price_step = self.findChild(QtWidgets.QLineEdit, "entry_price_step")
 
-        self.entry_mcost_min = self.findChild(QtWidgets.QLineEdit, "entry_mcost_step")
-        self.entry_mcost_max = self.findChild(QtWidgets.QLineEdit, "entry_mcost_step")
+        self.entry_mcost_min = self.findChild(QtWidgets.QLineEdit, "entry_mcost_min")
+        self.entry_mcost_max = self.findChild(QtWidgets.QLineEdit, "entry_mcost_max")
         self.entry_mcost_step = self.findChild(QtWidgets.QLineEdit, "entry_mcost_step")
 
-        self.entry_pcost_min = self.findChild(QtWidgets.QLineEdit, "entry_pcost_step")
-        self.entry_pcost_max = self.findChild(QtWidgets.QLineEdit, "entry_pcost_step")
+        self.entry_pcost_min = self.findChild(QtWidgets.QLineEdit, "entry_pcost_min")
+        self.entry_pcost_max = self.findChild(QtWidgets.QLineEdit, "entry_pcost_max")
         self.entry_pcost_step = self.findChild(QtWidgets.QLineEdit, "entry_pcost_step")
 
-        self.entry_recovery_min = self.findChild(QtWidgets.QLineEdit, "entry_recovery_step")
-        self.entry_recovery_max = self.findChild(QtWidgets.QLineEdit, "entry_recovery_step")
+        self.entry_recovery_min = self.findChild(QtWidgets.QLineEdit, "entry_recovery_min")
+        self.entry_recovery_max = self.findChild(QtWidgets.QLineEdit, "entry_recovery_max")
         self.entry_recovery_step = self.findChild(QtWidgets.QLineEdit, "entry_recovery_step")
 
-        self.entry_discount_min = self.findChild(QtWidgets.QLineEdit, "entry_discount_step")
-        self.entry_discount_max = self.findChild(QtWidgets.QLineEdit, "entry_discount_step")
+        self.entry_discount_min = self.findChild(QtWidgets.QLineEdit, "entry_discount_min")
+        self.entry_discount_max = self.findChild(QtWidgets.QLineEdit, "entry_discount_max")
         self.entry_discount_step = self.findChild(QtWidgets.QLineEdit, "entry_discount_step")
 
-        self.entry_scost_min = self.findChild(QtWidgets.QLineEdit, "entry_scost_step")
-        self.entry_scost_max = self.findChild(QtWidgets.QLineEdit, "entry_scost_step")
+        self.entry_scost_min = self.findChild(QtWidgets.QLineEdit, "entry_scost_min")
+        self.entry_scost_max = self.findChild(QtWidgets.QLineEdit, "entry_scost_max")
         self.entry_scost_step = self.findChild(QtWidgets.QLineEdit, "entry_scost_step")
 
         self.button_3d_plot = self.findChild(QtWidgets.QPushButton, "button_3d_plot")
@@ -242,5 +244,155 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         Esta función inicia el proceso de las iteraciones.
         """
-        print("Boton iteraciones")
+        if self.alert_message_iterations():
+            print("Ok")
+        else:
+            print("Nao Nao")
+        
         return
+    
+
+    def alert_message_iterations(self):
+        """
+        Esta función muestra una alerta a la hora de crear las iteraciones, ya que es un proceso que puede
+        demorar mucho y generar muchos Gb en archivos.
+        """
+        file_number, file_size = self.iterations_result_files()
+        self.number_of_files = file_number
+
+        string_file_number = f"{file_number:,}".replace(",", ".")
+
+        if file_size < 1024:
+            string_files_size = f'{file_size:,.2f}'.replace(',', '.')
+            string_files_size += ' Gb'
+        else:
+            string_files_size = f'{(file_size/1024):,.2f}'.replace(',', '.')
+            string_files_size += ' Tb'
+        
+        iterate_time = self.convert_time(file_number*0.25)
+
+        mensaje = QMessageBox(self)
+        mensaje.setWindowTitle("Confirmación")
+        mensaje.setText(
+            "¿Estás seguro?\n"
+            f"Se crearán {string_file_number} archivos.\n"
+            f"El peso total será {string_files_size} aproximadamente.\n"
+            f"Demorará aproximadamente {iterate_time}")
+        mensaje.setIcon(QMessageBox.Icon.Warning)
+        mensaje.setStandardButtons(QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel)
+        
+        respuesta = mensaje.exec()
+
+        return respuesta == QMessageBox.StandardButton.Ok
+    
+
+    def iterations_result_files(self):
+        """
+        Entrega el número de archivos que se crearán al realizar las iteraciones, además de un aproximado
+        del peso de la carpeta que los contendrá.
+        """
+        self.list_entries = []
+        self.list_entries_append()
+        print(self.list_entries)
+
+        elements_in_range = []
+
+        for elem in self.list_entries:
+            elements_in_range.append(self.define_list(elem[0], elem[1], elem[2]))
+        
+        total_length = len(elements_in_range[0])*len(elements_in_range[1])*len(elements_in_range[2])*len(elements_in_range[3])*len(elements_in_range[4])*len(elements_in_range[5])
+
+        return total_length, ((total_length + 5)*1200/1024)/1024
+    
+
+    def define_list(self, min:int|float, max:int|float, step:int|float):
+        """
+        Funcion que crea una lista desde un valor minimo hasta un valor máximo con un paso definido.
+        
+        Inputs:
+        * min: Valor minimo de la lista.
+        * max: Valor maximo de la lista.
+        * ste: salto entre cada elemento de la lista.
+        
+        Outputs:
+        * Si los valores de minimo y máximo son iguales, retorna una lista con 1 elemento (el minimo).
+        Si el minimo y el máximo son distintos, crea una lista con n elementos, con n igual al espacio
+        entre el min y max con un salto igual a step.
+
+        Ejemplo de uso:
+        >>> self.define_list(3, 9, 1)
+        [3, 4, 5, 6, 7, 8, 9]
+
+        >>> self.define_list(5, 5, 1)
+        [5]
+        """
+        if min == max:
+            return [min]
+        else:
+            return np.arange(min, max + step, step)
+
+
+    def list_entries_append(self):
+        """
+        Agrega las entradas a una lista para su posterior uso.
+        """
+        self.list_entries.append([])
+        self.list_entries[0].append(float(self.entry_price_min.text()))
+        self.list_entries[0].append(float(self.entry_price_max.text()))
+        self.list_entries[0].append(float(self.entry_price_step.text()))
+
+        self.list_entries.append([])
+        self.list_entries[1].append(float(self.entry_mcost_min.text()))
+        self.list_entries[1].append(float(self.entry_mcost_max.text()))
+        self.list_entries[1].append(float(self.entry_mcost_step.text()))
+
+        self.list_entries.append([])
+        self.list_entries[2].append(float(self.entry_pcost_min.text()))
+        self.list_entries[2].append(float(self.entry_pcost_max.text()))
+        self.list_entries[2].append(float(self.entry_pcost_step.text()))
+
+        self.list_entries.append([])
+        self.list_entries[3].append(float(self.entry_discount_min.text()))
+        self.list_entries[3].append(float(self.entry_discount_max.text()))
+        self.list_entries[3].append(float(self.entry_discount_step.text()))
+
+        self.list_entries.append([])
+        self.list_entries[4].append(float(self.entry_recovery_min.text()))
+        self.list_entries[4].append(float(self.entry_recovery_max.text()))
+        self.list_entries[4].append(float(self.entry_recovery_step.text()))
+
+        self.list_entries.append([])
+        self.list_entries[5].append(float(self.entry_scost_min.text()))
+        self.list_entries[5].append(float(self.entry_scost_max.text()))
+        self.list_entries[5].append(float(self.entry_scost_step.text()))
+        return
+    
+
+    def convert_time(self, time: int):
+        """
+        Funcion que transforma un tiempo en segundos a dias, horas, minutos, segundos.
+        
+        Inputs:
+            * time: Tiempo en segundos.
+        
+        Outputs:
+            * string_time: String que contiene el tiempo de la siguiente forma: [  ]d [  ]h [  ]m [  ]s
+        
+        Ejemplo de uso:
+            >>> self.convert_time(90061)
+            "1d 1h 1m 1s"
+        """
+        days = time // 86400
+        time %= 86400
+        hours = time // 3600
+        time %= 3600
+        minutes = time // 60
+        time %= 60
+        string_time_days = f'{int(days):,}'.replace(',', '.')
+        string_time_hours = f'{int(hours):,}'.replace(',', '.')
+        string_time_minutes = f'{int(minutes):,}'.replace(',', '.')
+        string_time_seconds = f'{int(time):,}'.replace(',', '.')
+        string_time = f'{string_time_days}d {string_time_hours}h {string_time_minutes}m {string_time_seconds}s'
+        return string_time
+
+
