e38a25bfa77bc67a222db4e74b831c01ea5ba358
Cristobal Quiroz <cristobal.quiroz17@gmail.com>
2025-02-04 13:13:45 -0300

    feature / Añadida una tabla con estadisticas en la ventana del footprint

--- view/fp_window.py
+++ view/fp_window.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt                                                 
 
 from PyQt6 import QtWidgets, uic                                                            # 
 from PyQt6.QtWidgets import QFileDialog, QMessageBox, QGraphicsScene                        # Paquetes parte de PyQt6 > QtWidgets que ayudan a 
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
 from matplotlib.figure import Figure                                                        # 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas            # 
 
@@ -25,18 +26,30 @@ class FootprintWindow(QtWidgets.QDialog):
 
         self.create_plot_window()
 
+        self.define_table()
+
 
     def create_control_variables(self):
         """
         Esta función crea las variables de control que se usarán en la ventana.
         """
-        self.dataframes = []
-        self.coordinates = []
-        self.heights = []
-        self.states = []
-        self.plot_labels = ["Best case", "Mid case (75%)", "Mid case (50%)", "Mid case (25%)", "Worst case"]
-        self.plot_colors = ['#00b894', '#ffeaa7', '#0984e3', '#ff7675', '#636e72']
-        self.plot_sizes = [375, 300, 225, 150, 75]
+        # Variables generales de la ventana.
+        self.dataframes =   []
+        self.coordinates =  []
+        self.heights =      []
+        self.states =       []
+
+        # Variables para el gráfico.
+        self.plot_labels =  ["Best case", "Mid case (75%)", "Mid case (50%)", "Mid case (25%)", "Worst case"]
+        self.plot_colors =  ['#00b894', '#ffeaa7', '#0984e3', '#ff7675', '#636e72']
+        self.plot_sizes =   [375, 300, 225, 150, 75]
+
+        # Variables generales para la tabla
+        self.table_data =       []
+        self.table_van =        []
+        self.table_column_q =   []
+        self.table_tonnage =    []
+        self.table_grade =      []
         return
 
 
@@ -55,6 +68,8 @@ class FootprintWindow(QtWidgets.QDialog):
         self.checkboxes = [self.check_best, self.check_mid75, self.check_mid50, self.check_mid25, self.check_worst]
 
         self.button_refresh = self.findChild(QtWidgets.QPushButton, "button_guardar")
+
+        self.stats_table = self.findChild(QtWidgets.QTableView, "table_statistics")
         return
 
 
@@ -172,4 +187,70 @@ class FootprintWindow(QtWidgets.QDialog):
         self.coordinates = []
         for df in self.dataframes:
             self.coordinates.append(df[df['z'] == int(self.cota_z.currentText())])
+        return
+    
+
+    def define_table(self):
+        """
+        Esta función crea la tabla donde se mostrarán los datos estadísticos
+        de cada caso de estudio.
+        Para este caso, las columnas serán:
+        * Nombre.
+        * VAN.
+        * Q Columnas.
+        * Tonelaje.
+        * Ley promedio.
+        """
+        self.model = QStandardItemModel(5, 5)
+        self.model.setHorizontalHeaderLabels(["Caso", "VAN [MUSD]", "Nro. Columnas", "Tonelaje [Mt]", "Ley promedio [%]"])
+
+        self.define_table_params()
+        self.params_to_table()
+
+        for row_index, row in enumerate(self.table_data):
+            for col_index, value in enumerate(row):
+                item = QStandardItem(str(value))
+                self.model.setItem(row_index, col_index, item)
+        
+        self.stats_table.setModel(self.model)
+        return
+    
+
+    def define_table_params(self):
+        """
+        Esta función define los parámetros estadísticos que se obtendrán en cada uno de los casos de estudio.
+        """
+        for df in self.dataframes:
+            column_dataframes = []
+            column_sum = []
+            grouped = df.groupby(['x', 'y'])
+            ton = 1000*2.7
+
+            for _, group in grouped:
+                column_dataframes.append(group)
+                group['valor_acum'] = group['disc_value'].cumsum()
+                max_van = group['valor_acum'].max()
+                column_sum.append(max_van)
+            
+            case_van = round(sum(column_sum) / 1_000_000, 2)
+            self.table_van.append(case_van)
+
+            case_column_q = len(grouped)
+            self.table_column_q.append(case_column_q)
+
+            case_tonnage = round(df.shape[0]*ton/1_000_000, 2)
+            self.table_tonnage.append(case_tonnage)
+
+            case_grade = round(df['grade'].sum()/df.shape[0], 5)
+            self.table_grade.append(case_grade)
+        return
+    
+
+    def params_to_table(self):
+        """
+        Esta función define la variable table_data, agregando los valores obtenidos por la función >>> self.define_table_params().
+        """
+        self.table_data = []
+        for name, van, q, ton, g in zip(self.plot_labels, self.table_van, self.table_column_q, self.table_tonnage, self.table_grade):
+            self.table_data.append([name, van, q, ton, g])
         return
